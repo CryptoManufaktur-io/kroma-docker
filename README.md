850e998d12c1de2c959c6cf7105e518e6c32700c
Yorick Downe <yorickdowne@gmail.com>
2023-11-03 14:29:24 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# kroma-docker
+# Overview
+
 Docker Compose for Kroma RPC node
+
+`cp default.env .env`, then `nano .env` and adjust values for the right network including bootnodes.
+
+Meant to be used with [central-proxy-docker](https://github.com/CryptoManufaktur-io/central-proxy-docker) for traefik
+and Prometheus remote write; use `:ext-network.yml` in `COMPOSE_FILE` inside `.env` in that case.
+
+If you want the kroma-geth RPC ports exposed locally, use `rpc-shared.yml` in `COMPOSE_FILE` inside `.env`.
+
+The `./kromad` script can be used as a quick-start:
+
+`./kromad install` brings in docker-ce, if you don't have Docker installed already.
+
+`cp default.env .env`
+
+`nano .env` and adjust variables as needed, particularly `NETWORK` and `GETH_BOOT_NODES`, `NODE_BOOT_NODES`, as well
+as `L1_RPC` and `L1_RPC_KIND`.
+
+`./kromad up`
+
+To update the software, run `./kromad update` and then `./kromad up`
+
+This is Kroma Docker v1.0.0
---
+++ kroma-geth/docker-entrypoint.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -f /var/lib/kroma-geth/ee-secret/jwtsecret ]]; then
+  echo "Generating JWT secret"
+  __secret1=$(head -c 8 /dev/urandom | od -A n -t u8 | tr -d '[:space:]' | sha256sum | head -c 32)
+  __secret2=$(head -c 8 /dev/urandom | od -A n -t u8 | tr -d '[:space:]' | sha256sum | head -c 32)
+  echo -n "${__secret1}""${__secret2}" > /var/lib/kroma-geth/ee-secret/jwtsecret
+fi
+
+if [[ -O "/var/lib/kroma-geth/ee-secret/jwtsecret" ]]; then
+  chmod 666 /var/lib/kroma-geth/ee-secret/jwtsecret
+fi
+
+# Set verbosity
+shopt -s nocasematch
+case ${LOG_LEVEL} in
+  error)
+    __verbosity="--verbosity 1"
+    ;;
+  warn)
+    __verbosity="--verbosity 2"
+    ;;
+  info)
+    __verbosity="--verbosity 3"
+    ;;
+  debug)
+    __verbosity="--verbosity 4"
+    ;;
+  trace)
+    __verbosity="--verbosity 5"
+    ;;
+  *)
+    echo "LOG_LEVEL ${LOG_LEVEL} not recognized"
+    __verbosity=""
+    ;;
+esac
+
+if [ ! -d /var/lib/kroma-geth/geth/chaindata ]; then
+    echo "Initializing from genesis."
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+    geth ${__verbosity} init --datadir=/var/lib/kroma-geth /var/lib/kroma-geth/config/genesis.json
+fi
+
+case "${NETWORK}" in
+    mainnet) __chainid="--networkid 255";;
+    sepolia) __chainid="--networkid 2358";;
+    *) echo "Unsupported network ${NETWORK}, aborting."; exit 1;;
+esac
+
+if [ -f /var/lib/kroma-geth/prune-marker ]; then
+  rm -f /var/lib/kroma-geth/prune-marker
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec ${__chainid} "$@" snapshot prune-state
+else
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec "$@" ${__chainid} ${__verbosity} ${EL_EXTRAS}
+fi
